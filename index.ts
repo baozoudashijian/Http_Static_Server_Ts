17ad05d92a7d5721ccb89ba24baad8ab9093bc89
Roger Zhang <1787009521@qq.com>
2022-04-27 23:45:05 +0800

    feat: 实现简单服务器

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log(1)
+import * as http from 'http';
+
+const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+    console.log(req.constructor)
+    console.log(res.constructor)
+    res.end('hello world 123')
+})
+
+server.listen(8000)
