24b65afbb2ea2b5a38c18c1cba2aabed68c1f334
Roger Zhang <1787009521@qq.com>
2022-04-28 22:44:20 +0800

    feat: nodejs接受post请求

--- index.ts
+++ index.ts
@@ -1,9 +1,24 @@
 import * as http from 'http';
 
-const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-    console.log(req.constructor)
-    console.log(res.constructor)
+const server = http.createServer()
+
+server.on('request', (req: http.IncomingMessage, res: http.ServerResponse) => {
+    
     res.end('hello world 123')
+
+    let body = ""
+    req.on('data', (chunk) => {
+        // 会将buffer对象 隐式的转换成string
+        body += chunk
+        console.log(chunk, 'chunk')
+        console.log(typeof body, 'body')
+    })
+
+    req.on('end', () => {
+        console.log(body)
+        console.log(typeof body)
+    })
 })
+
 
 server.listen(8000)
