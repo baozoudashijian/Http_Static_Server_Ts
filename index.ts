ccd3c6db0e45d5609cd4567b2f462f2d46513183
Roger Zhang <1787009521@qq.com>
2022-04-28 23:19:22 +0800

    feat: 实现读取文件的方式返回内容

--- index.ts
+++ index.ts
@@ -1,22 +1,53 @@
+import * as fs from 'fs';
 import * as http from 'http';
+import * as url from 'url'
+import * as path from 'path'
 
 const server = http.createServer()
 
 server.on('request', (req: http.IncomingMessage, res: http.ServerResponse) => {
-    
-    res.end('hello world 123')
+
+    const p = url.parse(req.url).path
+    const publicDir = path.join(__dirname, '/public')
+
+    switch (p) {
+        case '/index.html':
+            fs.readFile(path.join(publicDir, '/index.html'), (err, data) => {
+                if (err) {
+                    console.log(err)
+                    return false
+                }
+                res.end(data.toString())
+            })
+            break;
+        case '/index.css':
+            fs.readFile(path.join(publicDir, '/index.css'), (err, data) => {
+                if (err) {
+                    console.log(err)
+                    return false
+                }
+                res.end(data.toString())
+            })
+            break;
+        case '/index.js':
+            fs.readFile(path.join(publicDir, '/index.js'), (err, data) => {
+                if (err) {
+                    console.log(err)
+                    return false
+                }
+                res.end(data.toString())
+            })
+            break;
+    }
 
     let body = ""
     req.on('data', (chunk) => {
         // 会将buffer对象 隐式的转换成string
         body += chunk
-        console.log(chunk, 'chunk')
-        console.log(typeof body, 'body')
     })
 
     req.on('end', () => {
-        console.log(body)
-        console.log(typeof body)
+
     })
 })
 
